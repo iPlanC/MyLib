1e1a2a62a83e7af0e08d824b829a0e66607bf1c3
PlanC <planc2333@outlook.com>
2020-08-04 10:05:32 +0800

    Update mylib.h

--- mylib.h
+++ mylib.h
@@ -3,16 +3,41 @@
 
 #define BOOL	int
 #define TRUE	1
-#define FALSE   0
+#define FALSE	0
 
-int*	Bubble_sort	(int values[], int reverse);
+char*	Alphabet	(BOOL bigger, BOOL reverse);
+int*	Bubble_sort	(int values[], BOOL reverse);
 int		Max_int		(int values[]);
 int		Min_int		(int values[]);
 
-int* Bubble_sort(int values[], BOOL reverse) {
+char* Alphabet(BOOL bigger, BOOL reverse) {
+	int i = 0;
 	int temp = 0;
+	char start = 'A';
+	char* str = (char*)malloc(26 * sizeof(char));
+	if (bigger == TFALSE) {
+		start = 'a';
+	}
+	for (i = 0; i < 26; i++) {
+		strcat(str, (char)i);
+	}
+	if (reverse == FALSE) {
+		return str;
+	}
+	else {
+		for (i = 0; i <= sizeof(str) / sizeof(char) / 2; i++) {
+			temp = str[i];
+			str[i] = str[sizeof(str) / sizeof(char) - i];
+			str[sizeof(str) / sizeof(char) - i] = temp;
+		}
+		return str;
+	}
+}
+
+int* Bubble_sort(int values[], BOOL reverse) {
 	int i = 0;
 	int j = 0;
+	int temp = 0;
 	for (i = 0; i <= sizeof(values) / sizeof(int) - 1; i++) {
 		for (j = 0; j <= sizeof(values) / sizeof(int) - i - 1; j++) {
 			if (values[j] > values[j + 1]) {
