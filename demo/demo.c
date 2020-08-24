df973e983d0cf4d71f32c6bddba3d431f7d6ca08
iPlanC <planc2333@outlook.com>
2020-08-24 09:54:51 +0800

    big update

--- demo/demo.c
+++ demo/demo.c
@@ -1,29 +1,61 @@
 /*
  * @Author: PlanC
  * @Date: 2020-08-20 13:53:50
- * @LastEditTime: 2020-08-20 14:17:44
+ * @LastEditTime: 2020-08-24 09:48:22
  * @FilePath: \MyLib\demo\demo.c
  */
 
 #include "../mylib.h"
 
 int main() {
+	// alphabet()
+	printf("%s\n", alphabet(1, 0));
+	// ABCDEFGHIJKLMNOPQRSTUVWXYZ
+	printf("%s\n", alphabet(0, 1));
+	// zyxwvutsrqponmlkjihgfedcba
+
+	// bubble_sort()
 	int i = 0;
 	int *p;
 	int *q;
-	int num[3] = {16, 35, 7};
-	printf("%d %d", Max_int(num), Min_int(num));
-	printf("\n");
-
 	int num1[3] = {1, 3, 2};
 	int num2[3] = {4, 6, 5};
-	p = Bubble_sort(num1, 0);
-	q = Bubble_sort(num2, 1);
+	p = bubble_sort(num1, 3, 0);
+	q = bubble_sort(num2, 3, 1);
 	for (i = 0; i < 3; i++) {
 		printf("%d ", *p++);
+		// 1 2 3
 	}
 	printf("\n");
 	for (i = 0; i < 3; i++) {
 		printf("%d ", *q++);
+		// 6 5 4
 	}
+	printf("\n");
+
+	// lengthint()
+	printf("%d\n", lengthint(1000));
+	// 4
+
+	// max_int() min_int()
+	int num[3] = {16, 35, 7};
+	printf("%d %d\n", max_int(num, 3), min_int(num, 3));
+	// 35 16
+
+	// stoi()
+	printf("%d\n", stoi("1001"));
+	// 1001(int)
+
+	// itos()
+	printf("%s\n", itos(1002));
+	// 1002(char*)
+
+	// toUpper()
+	char *str = "Hello";
+	printf("%s ", toUpper(str));
+	printf("%s ", toLower(str));
+	printf("\n")
+	// HELLO hello
+
+	return 0;
 }
--- mylib.h
+++ mylib.h
@@ -1,58 +1,61 @@
 /*
  * @Author: PlanC
  * @Date: 2020-06-10 12:27:23
- * @LastEditTime: 2020-08-20 15:00:25
+ * @LastEditTime: 2020-08-24 09:46:33
  * @FilePath: \MyLib\mylib.h
  */
 
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
-#include <system.h>
+#include <stdlib.h>
 
 #define BOOL	int
 #define TRUE	1
 #define FALSE	0
 
 char*	alphabet	(BOOL bigger, BOOL reverse);
-int*	bubble_sort	(int values[], BOOL reverse);
+int*	bubble_sort	(int *values, int length, BOOL reverse);
 int		lengthint	(int number);
-int		max_int		(int values[]);
-int		min_int		(int values[]);
+int		max_int		(int *values, int length);
+int		min_int		(int *values, int length);
 int		stoi		(char *str);
 char* 	itos		(int number);
-void	toUpper		(char *str);
-void	toLower		(char *str);
+char*	toUpper		(char *str);
+char*	toLower		(char *str);
 
 char* alphabet(BOOL bigger, BOOL reverse) {
 	int i = 0;
+	int j = 0;
 	int temp = 0;
 	char start = 'A';
-	char* str = (char*)malloc(26 * sizeof(char));
-	if (bigger == TFALSE) {
+	char *str = (char*)malloc(26 * sizeof(char));
+	if (bigger == FALSE) {
 		start = 'a';
 	}
 	for (i = 0; i < 26; i++) {
-		strcat(str, (char)i);
+		str[j++] = (char)(i + start);
 	}
+	str[j++] = '\0';
 	if (reverse == FALSE) {
 		return str;
 	}
 	else {
-		for (i = 0; i <= sizeof(str) / sizeof(char) / 2; i++) {
+		for (i = 0; i <= strlen(str) / 2; i++) {
 			temp = str[i];
-			str[i] = str[sizeof(str) / sizeof(char) - i];
-			str[sizeof(str) / sizeof(char) - i] = temp;
+			str[i] = str[strlen(str) - i - 1];
+			str[strlen(str) - i - 1] = temp;
 		}
 		return str;
 	}
 }
 
-int* bubble_sort(int values[], BOOL reverse) {
+int* bubble_sort(int *values,int length, BOOL reverse) {
 	int i = 0;
 	int j = 0;
 	int temp = 0;
-	for (i = 0; i <= sizeof(values) / sizeof(int) - 1; i++) {
-		for (j = 0; j <= sizeof(values) / sizeof(int) - i - 1; j++) {
+	for (i = 0; i <= length - 1; i++) {
+		for (j = 0; j <= length - i - 1; j++) {
 			if (values[j] > values[j + 1]) {
 				temp = values[j];
 				values[j] = values[j + 1];
@@ -64,10 +67,10 @@ int* bubble_sort(int values[], BOOL reverse) {
 		return values;
 	}
 	else {
-		for (i = 0; i <= sizeof(values) / sizeof(int) / 2; i++) {
+		for (i = 0; i <= length / 2; i++) {
 			temp = values[i];
-			values[i] = values[sizeof(values) / sizeof(int) - i];
-			values[sizeof(values) / sizeof(int) - i] = temp;
+			values[i] = values[length - i];
+			values[length - i] = temp;
 		}
 		return values;
 	}
@@ -76,22 +79,22 @@ int* bubble_sort(int values[], BOOL reverse) {
 int lengthint(int number) {
 	int count = 0;
 	while ((number/=10) != 0) count++;
-	reutrn count;
+	return ++count;
 }
 
-int max_int(int values[]) {
+int max_int(int *values, int length) {
 	int i = 0;
 	int temp_max = values[0];
-	for (i = 0; i <= sizeof(values) / sizeof(int); i++) {
+	for (i = 0; i <= length; i++) {
 		if (temp_max < values[i]) temp_max = values[i];
 	}
 	return temp_max;
 }
 
-int min_int(int values[]) {
+int min_int(int *values, int length) {
 	int i = 0;
 	int temp_min = values[0];
-	for (i = 0; i <= sizeof(values) / sizeof(int); i++) {
+	for (i = 0; i <= length; i++) {
 		if (temp_min > values[i]) temp_min = values[i];
 	}
 	return temp_min;
@@ -115,28 +118,40 @@ int stoi(char *str) {
 
 char* itos(int number) {
 	int i = 0;
+	int j = 0;
+	int temp = 0;
 	int length = lengthint(number);
-	char* str[length] = {'\0'};
-	while (number / 10 != 0) {
-		str[i++] = number / 10;
+	char *str = (char*)malloc(length * sizeof(char) + 1);
+	if (number < 0) str[i++] = '-';
+	do {
+		str[i++] = number % 10 + '0';
+
+	} while (number/=10);
+	for (j = 0; j <= strlen(str) / 2; j++) {
+		temp = str[j];
+		str[j] = str[strlen(str) - j - 1];
+		str[strlen(str) - j - 1] = temp;
 	}
+	str[i++] = '\0';
 	return str;
 }
 
-void toUpper(char *str) {
+char* toUpper(char *str) {
 	int i = 0;
 	for (i = 0; i < strlen(str); i++) {
 		if (str[i] >= 97 && str[i] <= 122) {
 			str[i] = str[i] - 32;
 		}
 	}
+	return str;
 }
 
-void toLower(char *str) {
+char* toLower(char *str) {
 	int i = 0;
 	for (i = 0; i < strlen(str); i++) {
 		if (str[i] >= 65 && str[i] <= 90) {
 			str[i] = str[i] + 32;
 		}
 	}
+	return str;
 }
